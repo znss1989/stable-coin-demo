25ef7913176e49d50cb1495f234c47e9a3224034
Wu Lei <znss1989@gmail.com>
2019-01-09 15:24:49 +0800

    empty connection status ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Header, Divider } from 'semantic-ui-react';
 
 import './App.css';
+import ConnectionStatus from './components/ConnectionStatus';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMetaMask: false,
+      isUnlocked: false,
+      network: '',
+      currentAccount: ''
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +22,7 @@ class App extends Component {
           <br />
           <Header as='h1'>Stable Coins Dashboard</Header>
           <Divider horizontal>Connection Status</Divider>
+          <ConnectionStatus />
           <Divider horizontal>Fiat Selection</Divider>
           <Divider horizontal>App Dashboard</Divider>
           <footer>
@@ -20,6 +32,20 @@ class App extends Component {
       </div>
     );
   }
+
+  // async componentDidMount() {
+  //   const isMetaMask = statusService.isMetaMask();
+  //   const network = await statusService.getNetwork();
+  //   const accounts = await statusService.getAccounts();
+  //   const isUnlocked = accounts.length > 0;
+  //   const currentAccount = accounts[0];
+  //   this.setState({
+  //     isMetaMask,
+  //     network,
+  //     isUnlocked,
+  //     currentAccount
+  //   });
+  // }
 }
 
 export default App;
